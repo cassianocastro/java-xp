8c2cfdac38ad703a35237ab077e947471eafc6f2
cassianocastro <tottenham248@gmail.com>
2022-05-14 17:57:36 -0300

    Removed the try/cacth clause and added the "throws" clause.

--- src/dataBaseIntegration/ConnectionFactory.java
+++ src/dataBaseIntegration/ConnectionFactory.java
@@ -1,19 +1,15 @@
 package dataBaseIntegration;
+
 import java.sql.*;
 
 /**
- * Fábrica de Conexões com a Base de Dados "exemplo".
- * @author cassiano
+ *
  */
-public class ConnectionFactory{
+public class ConnectionFactory
+{
 
-    public Connection getConnection(){
-        try{
-            return DriverManager.getConnection(
-                    "jdbc:mysql://localhost/exemplo", "root", "");
-        }catch( SQLException e ){
-            throw new RuntimeException(e);
-        }
-
+    public Connection getConnection() throws SQLException
+    {
+        return DriverManager.getConnection("jdbc:sqlite:src/lib/db.sqlite");
     }
 }

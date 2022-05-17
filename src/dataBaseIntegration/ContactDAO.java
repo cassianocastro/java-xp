8e48ce1882e62f5c345872ecef48806d979cfc55
cassianocastro <tottenham248@gmail.com>
2022-05-17 15:19:47 -0300

    Added the try-with-resources clauses in the methods for closing of the streams. Used type "var" instead of explicit types.

--- src/dataBaseIntegration/ContactDAO.java
+++ src/dataBaseIntegration/ContactDAO.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 /**
  *
+ * 
  */
 public class ContactDAO
 {
@@ -20,84 +21,92 @@ public class ContactDAO
 
     public void insert(Contact contact) throws SQLException
     {
-        PreparedStatement statement = this.connection.prepareStatement(
+        try (var statement = this.connection.prepareStatement(
             "INSERT INTO contact(name, email, address, birthDate) VALUES (?, ?, ?, ?)"
-        );
-        statement.setString(1, contact.getName());
-        statement.setString(2, contact.getEmail());
-        statement.setString(3, contact.getAddress());
-        statement.setDate(4, new Date(contact.getBirthDate().getTimeInMillis()));
+        ))
+        {
+            statement.setString(1, contact.getName());
+            statement.setString(2, contact.getEmail());
+            statement.setString(3, contact.getAddress());
+            statement.setDate(4, new Date(contact.getBirthDate().getTimeInMillis()));
 
-        statement.execute();
+            statement.execute();
+        }
     }
 
     public List read() throws SQLException
     {
-        PreparedStatement statement = this.connection.prepareStatement(
-            "SELECT id, name, email, address, birthDate FROM contact"
-        );
-        ResultSet rs = statement.executeQuery();
         List<Contact> list = new ArrayList<>();
 
-        while ( rs.next() )
+        try (var statement = this.connection.prepareStatement(
+            "SELECT id, name, email, address, birthDate FROM contact"
+        );
+            var rs = statement.executeQuery())
         {
-            Long id            = rs.getLong("id");
-            String name        = rs.getString("name");
-            String email       = rs.getString("email");
-            String address     = rs.getString("address");
-            Calendar birthDate = Calendar.getInstance();
-            birthDate.setTime(rs.getDate("birthDate"));
+            while ( rs.next() )
+            {
+                Long id            = rs.getLong("id");
+                String name        = rs.getString("name");
+                String email       = rs.getString("email");
+                String address     = rs.getString("address");
+                Calendar birthDate = Calendar.getInstance();
+                birthDate.setTime(rs.getDate("birthDate"));
 
-            list.add(new Contact(id, name, email, address, birthDate));
+                list.add(new Contact(id, name, email, address, birthDate));
+            }
         }
-
         return list;
     }
 
     public Contact findByID(Long ID) throws SQLException
     {
-        PreparedStatement statement = this.connection.prepareStatement(
+        try (var statement = this.connection.prepareStatement(
             "SELECT id, name, email, address, birthDate FROM contact WHERE id = ?"
-        );
-        statement.setString(1, ID.toString());
-        ResultSet rs = statement.executeQuery();
-
-        if ( rs.next() )
+        ))
         {
-            Long id            = rs.getLong("id");
-            String name        = rs.getString("name");
-            String email       = rs.getString("email");
-            String address     = rs.getString("address");
-            Calendar birthDate = Calendar.getInstance();
-            birthDate.setTime(rs.getDate("birthDate"));
+            statement.setString(1, ID.toString());
+            try (var rs = statement.executeQuery())
+            {
+                if ( rs.next() )
+                {
+                    Long id            = rs.getLong("id");
+                    String name        = rs.getString("name");
+                    String email       = rs.getString("email");
+                    String address     = rs.getString("address");
+                    Calendar birthDate = Calendar.getInstance();
+                    birthDate.setTime(rs.getDate("birthDate"));
 
-            return new Contact(id, name, email, address, birthDate);
+                    return new Contact(id, name, email, address, birthDate);
+                }
+            }
         }
-
         throw new SQLException("Contact not found!");
     }
 
     public void update(Contact contact) throws SQLException
     {
-        PreparedStatement statement = this.connection.prepareStatement(
+        try (var statement = this.connection.prepareStatement(
             "UPDATE contact SET name = ?, email = ?, address = ?, birthDate = ? WHERE id = ?"
-        );
-        statement.setString(1, contact.getName());
-        statement.setString(2, contact.getEmail());
-        statement.setString(3, contact.getAddress());
-        statement.setDate(4, new Date(contact.getBirthDate().getTimeInMillis()));
-        statement.setLong(5, contact.getID());
+        ))
+        {
+            statement.setString(1, contact.getName());
+            statement.setString(2, contact.getEmail());
+            statement.setString(3, contact.getAddress());
+            statement.setDate(4, new Date(contact.getBirthDate().getTimeInMillis()));
+            statement.setLong(5, contact.getID());
 
-        statement.execute();
+            statement.executeUpdate();
+        }
     }
 
     public void delete(Contact contact) throws SQLException
     {
-        PreparedStatement statement = this.connection.prepareStatement(
+        try (java.sql.PreparedStatement statement = this.connection.prepareStatement(
             "DELETE FROM contact WHERE id = ?"
-        );
-        statement.setLong(1, contact.getID());
-
-        statement.execute();
+        ))
+        {
+            statement.setLong(1, contact.getID());
+            statement.execute();
+        }
     }
 }
